5b4914b74899095202104ea37ead5a96e02d25e4
Guy-Jeef TUMBA <guyjeef16@gmail.com>
2023-03-03 14:35:55 +0000

    Examen Blanc - Creation fichier des models

---
+++ app/Models/ExamenBlanc/Etablissement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\ExamenBlanc;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etablissement extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'examen_blanc__etablissements';
+}
---
+++ app/Models/ExamenBlanc/Examen.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\ExamenBlanc;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Examen extends Model
+{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'examen_blanc__examens';
+}
